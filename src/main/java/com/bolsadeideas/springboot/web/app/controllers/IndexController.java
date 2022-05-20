4cc826c1861593b5eee7f57eac1ae01681d14e6d
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-05-20 00:08:06 -0600

    feat(video 15): 15. El objeto Model

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
@@ -13,5 +15,18 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring con Model!");
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		
+		Usuario usuario = new Usuario();
+		
+		usuario.setNombre("Roberto");
+		usuario.setApellido("Flores");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		 
+		return "perfil";
 	}
 }
