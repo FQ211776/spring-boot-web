c2bf9ce68e59dfdb86cc05bb051b0b547c1e670e
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-05-21 10:40:22 -0600

    refactor(video 17): 17. Directiva each (foreach) de Thymeleaf

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -14,33 +15,39 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({"/index", "/","", "/home"})
+	@GetMapping({ "/index", "/", "", "/home" })
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring con Model!");
 		return "index";
 	}
-	
+
 	@RequestMapping("/perfil")
 	public String perfil(Model model) {
-		
+
 		Usuario usuario = new Usuario();
-		
+
 		usuario.setNombre("Andres");
 		usuario.setApellido("Guzman");
-		usuario.setEmail("andres@correo.com"); 
-		
+		usuario.setEmail("andres@correo.com");
+
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
-		 
+
 		return "perfil";
 	}
-	
+
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		List<Usuario> usuarios = new ArrayList<>();
+		Usuario usuario = new Usuario();
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Andres", "Guzman", "andres@correo.com"),
+				new Usuario("Jhon", "Doe", "jhon@correo.com"),
+				new Usuario("Jane", "Doe", "jane@correo.com"),
+				new Usuario("Tornado", "Roe", "roe@correo.com"));
+		
+
 		model.addAttribute("titulo", "listado de usuarios");
 		model.addAttribute("usuarios", usuarios);
-		 
+
 		return "listar";
 	}
 }
