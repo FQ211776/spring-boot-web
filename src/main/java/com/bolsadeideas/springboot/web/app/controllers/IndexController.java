9b2c80ce119699a199b3f96c15c56332e8121883
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-05-23 08:43:48 -0600

    May 23, 2022 8:43 AM

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,10 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-=======
-import java.util.Arrays;
->>>>>>> robert
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -17,58 +13,15 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-<<<<<<< HEAD
-	
-	@GetMapping({"/index", "/", "/home"})
-	public String index (Model model){
-		
-		model.addAttribute("titulo", "hola spring framwork");
-		
-=======
 
 	@GetMapping({ "/index", "/", "", "/home" })
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring con Model!");
->>>>>>> robert
 		return "index";
 	}
 
 	@RequestMapping("/perfil")
-<<<<<<< HEAD
-	
-	public String perfil(Model model) {
-		
-		Usuario usuario = new Usuario();
-		
-		usuario.setNombre("David");
-		usuario.setApellido("Driotis");
-		usuario.setEmail("david.driotis@gmail.com");
-		
-		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
-		
-		return "perfil";
-	}
-	
-@RequestMapping("/listar")
-	
-	public String listar(Model model) {
-	
-	List<Usuario> usuarios = new ArrayList<>();
-	
-	usuarios.add(new Usuario( "David", "Driotis", "david.driotis@gmail.com"));
-	usuarios.add(new Usuario( "Johny", "Cruz", "Johny.Cruz@gmail.com"));
-	usuarios.add(new Usuario( "Miguel", "Zelada", "Miguel.Zelada@gmail.com"));
-	usuarios.add(new Usuario( "Minor", "Martinez", "Minor.Martinez@gmail.com"));
 
-	model.addAttribute("titulo", "Listado de usuarios");
-	model.addAttribute("usuarios", usuarios);
-	return "listar";
-
-}
-}
-
-=======
 	public String perfil(Model model) {
 
 		Usuario usuario = new Usuario();
@@ -90,12 +43,12 @@ public class IndexController {
 				new Usuario("Jhon", "Doe", "jhon@correo.com"),
 				new Usuario("Jane", "Doe", "jane@correo.com"),
 				new Usuario("Tornado", "Roe", "roe@correo.com"));
-		
 
-		model.addAttribute("titulo", "listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
+
+	model.addAttribute("titulo", "Listado de usuarios");
+	model.addAttribute("usuarios", usuarios);
 
 		return "listar";
 	}
 }
->>>>>>> robert
+
