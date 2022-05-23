42430ab2dd42ef672bd7c512aa058c3af84def00
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-05-23 08:40:33 -0600

    May 23, 2022 8:40 AM

--- src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
@@ -4,78 +4,38 @@ public class Usuario {
 	private String nombre;
 	private String apellido;
 	private String email;
-<<<<<<< HEAD
-	
-	
-	
-	public Usuario() {
-	}
-	
-=======
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 	public Usuario() {
 	}
 
->>>>>>> robert
 	public Usuario(String nombre, String apellido, String email) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.email = email;
 	}
-<<<<<<< HEAD
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getApellido() {
 		return apellido;
 	}
-	
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-	
-=======
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
->>>>>>> robert
-
 }
