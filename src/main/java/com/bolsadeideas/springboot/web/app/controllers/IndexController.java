336ced71241abe9e81548ae7996bced5c6600f96
DavidDriotis <80718370+DavidDriotis@users.noreply.github.com>
2022-05-28 20:09:00 -0600

    -Video 19
    19. Anotacion @RequestParam

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -37,17 +38,21 @@ public class IndexController {
 	}
 
 	@RequestMapping("/listar")
-	public String listar(Model model) {
-		Usuario usuario = new Usuario();
+	public String listar(Model model) {		
+
+		model.addAttribute("titulo", "listado de usuarios");
+
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		
 		List<Usuario> usuarios = Arrays.asList(new Usuario("Andres", "Guzman", "andres@correo.com"),
 				new Usuario("Jhon", "Doe", "jhon@correo.com"),
 				new Usuario("Jane", "Doe", "jane@correo.com"),
 				new Usuario("Tornado", "Roe", "roe@correo.com"));
 		
-
-		model.addAttribute("titulo", "listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
-
-		return "listar";
+		return usuarios;
 	}
 }
