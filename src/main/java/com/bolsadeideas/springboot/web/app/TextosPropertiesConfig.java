06a4a246791262e1db90f27015ad7065a050ea94
hugo <HG212137@alumno.udb.edu.sv>
2022-06-10 20:24:15 -0600

    video 25 corregido

--- src/main/java/com/bolsadeideas/springboot/web/app/TextosPropertiesConfig.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/TextosPropertiesConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @PropertySources({
-	
 	@PropertySource("classpath:textos.properties")
+	
 	
 })
 public class TextosPropertiesConfig {
